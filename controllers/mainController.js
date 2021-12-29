15f689ff8522bbda8a204c852b6412b60e73eab9
G-Linares <gerrylinares@gmail.com>
2021-12-29 03:48:47 -0600

    finished structure and API fetches, ready to start CSS

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -3,9 +3,36 @@ const axios = require('axios');
 const mainController = {
     index : async(req, res) => {
         try {
-          const pokemonAPI = await axios.get(`https://pokeapi.co/api/v2/pokemon`)
-          console.log(pokemonAPI.data)
-          res.render('index', {pokemon : pokemonAPI.data});
+          
+          // const pokemonApi = await axios.get(`https://pokeapi.co/api/v2/pokemon/`)
+          // console.log(pokemonApi.data)
+          // res.render('index', {pokemon : pokemonApi.data});
+          
+          //fetch al API de PokeAPI
+          const pokemonApi = await axios.get("https://pokeapi.co/api/v2/pokemon");
+          const pokemonApiLength = pokemonApi.data.results.length;
+          const pokemonArray = [];
+          
+          for (let i = 0; i < pokemonApiLength; i++) {
+            const currentPoke = pokemonApi.data.results[i];  
+            //hago el fetch de nuevo para obtener datos de un cierto pokemon
+            const pokeImage = await axios.get(currentPoke.url);
+            //creo un array de objetos para almacenar todos sus datos
+            const newPokeObj = {
+              name: currentPoke.name,
+              image: pokeImage.data.sprites.other.dream_world.front_default,
+              id: pokeImage.data.id,
+              type: pokeImage.data.types[0].type.name,
+              weight: pokeImage.data.weight,
+              attack: pokeImage.data.stats[1].base_stat,
+              defense: pokeImage.data.stats[2].base_stat,
+              ability: pokeImage.data.abilities[0].ability.name,
+              
+            };
+            pokemonArray.push(newPokeObj);
+          }
+    
+          res.render("index", { pokemon: pokemonArray });  
   
         } catch (error) {
           if(error.response){
--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -1,6 +1,4 @@
-const { default: axios } = require('axios');
 var express = require('express');
-const res = require('express/lib/response');
 var mainRouter = express.Router();
 const mainController = require('../controllers/mainController');
 
