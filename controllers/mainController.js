ff4e82d533d0d881d14682efd4398f9c156d7b74
G-Linares <gerrylinares@gmail.com>
2021-12-29 13:48:18 -0600

    Added feature where you can see all the types from each pokemon

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -12,17 +12,39 @@ const mainController = {
           const pokemonApi = await axios.get("https://pokeapi.co/api/v2/pokemon");
           const pokemonApiLength = pokemonApi.data.results.length;
           const pokemonArray = [];
+          // const pokeAlltypes = [];
+
+          // function allTypes(pokemonApiData){
+            
+          //   // console.log(pokemonApiData);
+          //   for(let i = 0; i < pokemonApiData.lenght;i++){
+          //     // console.log(pokemonApiTypesData[i].types[0].type.name);
+          //     pokeAlltypes.push(pokemonApiData[i])
+          //   }
+          //   console.log(pokeAlltypes);
+          //   return pokeAlltypes;
+          // }
+
+          function secondType(secondType){
+            if(secondType){
+              return secondType.type.name;
+            } else return undefined;
+          }
           
           for (let i = 0; i < pokemonApiLength; i++) {
             const currentPoke = pokemonApi.data.results[i];  
             //hago el fetch de nuevo para obtener datos de un cierto pokemon
             const pokeImage = await axios.get(currentPoke.url);
             //creo un array de objetos para almacenar todos sus datos
+            
+
             const newPokeObj = {
               name: currentPoke.name,
               image: pokeImage.data.sprites.other.dream_world.front_default,
               id: pokeImage.data.id,
-              type: pokeImage.data.types[0].type.name,
+              type1: pokeImage.data.types[0].type.name,
+              // type2: pokeImage.data.types[1].type.name, 
+              type2: secondType(pokeImage.data.types[1]),
               weight: pokeImage.data.weight,
               attack: pokeImage.data.stats[1].base_stat,
               defense: pokeImage.data.stats[2].base_stat,
@@ -30,19 +52,20 @@ const mainController = {
               
             };
             pokemonArray.push(newPokeObj);
+            // console.log(newPokeObj);
           }
     
-          res.render("index", { pokemon: pokemonArray });  
+          res.render("index1", { pokemon: pokemonArray });  
   
         } catch (error) {
           if(error.response){
-            console.log(err.response.data);
-            console.log(err.response.status);
-            console.log(err.response.headers);
-          } else if(err.request){
-            console.log(err.request);
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+          } else if(error.request){
+            console.log(error.request);
           } else {
-            console.log('Error', err.message);
+            console.log('Error', error.message);
           }
         }
       }
