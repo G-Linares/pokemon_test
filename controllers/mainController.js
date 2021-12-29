8f37b54c814b8b78df7b4f991eadf314ef0f149c
G-Linares <gerrylinares@gmail.com>
2021-12-29 13:50:27 -0600

    Added feature where you can see all the types from each pokemon

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -12,19 +12,7 @@ const mainController = {
           const pokemonApi = await axios.get("https://pokeapi.co/api/v2/pokemon");
           const pokemonApiLength = pokemonApi.data.results.length;
           const pokemonArray = [];
-          // const pokeAlltypes = [];
-
-          // function allTypes(pokemonApiData){
-            
-          //   // console.log(pokemonApiData);
-          //   for(let i = 0; i < pokemonApiData.lenght;i++){
-          //     // console.log(pokemonApiTypesData[i].types[0].type.name);
-          //     pokeAlltypes.push(pokemonApiData[i])
-          //   }
-          //   console.log(pokeAlltypes);
-          //   return pokeAlltypes;
-          // }
-
+          
           function secondType(secondType){
             if(secondType){
               return secondType.type.name;
@@ -36,14 +24,12 @@ const mainController = {
             //hago el fetch de nuevo para obtener datos de un cierto pokemon
             const pokeImage = await axios.get(currentPoke.url);
             //creo un array de objetos para almacenar todos sus datos
-            
 
             const newPokeObj = {
               name: currentPoke.name,
               image: pokeImage.data.sprites.other.dream_world.front_default,
               id: pokeImage.data.id,
-              type1: pokeImage.data.types[0].type.name,
-              // type2: pokeImage.data.types[1].type.name, 
+              type1: pokeImage.data.types[0].type.name, 
               type2: secondType(pokeImage.data.types[1]),
               weight: pokeImage.data.weight,
               attack: pokeImage.data.stats[1].base_stat,
@@ -55,7 +41,7 @@ const mainController = {
             // console.log(newPokeObj);
           }
     
-          res.render("index1", { pokemon: pokemonArray });  
+          res.render("index", { pokemon: pokemonArray });  
   
         } catch (error) {
           if(error.response){
