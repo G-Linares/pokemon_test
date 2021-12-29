e573215cd84da1408099eede7b4190f90cf3ba27
G-Linares <gerrylinares@gmail.com>
2021-12-28 21:23:56 -0600

    API pulled and displayed on index view

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,7 +1,26 @@
+const axios = require('axios');
+
 const mainController = {
-    index : (req, res) => {
-        res.render('index');
+    index : async(req, res) => {
+        try {
+          const pokemonAPI = await axios.get(`https://pokeapi.co/api/v2/pokemon`)
+          console.log(pokemonAPI.data)
+          res.render('index', {pokemon : pokemonAPI.data});
+  
+        } catch (error) {
+          if(error.response){
+            console.log(err.response.data);
+            console.log(err.response.status);
+            console.log(err.response.headers);
+          } else if(err.request){
+            console.log(err.request);
+          } else {
+            console.log('Error', err.message);
+          }
+        }
       }
+      
+
 }
 
 
--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -1,8 +1,11 @@
+const { default: axios } = require('axios');
 var express = require('express');
+const res = require('express/lib/response');
 var mainRouter = express.Router();
 const mainController = require('../controllers/mainController');
 
 /* Ruta principal */
 mainRouter.get('/', mainController.index);
+
 
 module.exports = mainRouter;
