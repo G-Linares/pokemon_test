1e6b83d890e61a3c9f98ed1ea3f369301c49bf38
G-Linares <gerrylinares@gmail.com>
2021-12-30 02:25:36 -0600

    added burger menu and float button

--- public/JS/script.js
+++ public/JS/script.js
@@ -1,4 +1,19 @@
+
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
+
 function toggleButton(){
     var action = document.querySelector('.action');
     action.classList.toggle('active');
+}
+
+function toggleNavMenu(){
+    const navMenu = document.querySelector(".nav-menu");
+    const hamburger = document.querySelector(".hamburger");
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+
 }
